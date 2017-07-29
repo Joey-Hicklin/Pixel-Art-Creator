af35f1a2c789192ad7d150df48cc6fff5e2cf99c
root <root@ip-172-31-23-67.us-west-2.compute.internal>
2017-07-29 07:06:02 +0000

    fixed linking

--- src/components/app.js
+++ src/components/app.js
@@ -4,14 +4,11 @@ import ContextMenu from './contextMenu';
 
 
 let pixelCount = ((window.outerHeight-89)*window.outerWidth)/400;
-const makingSure = prompt("Do you really want to create " + pixelCount + " div elements?");
 let pixelContainer = [];
 console.log(ContextMenu.state.display);
 
-if(makingSure != null){
-	for (var i = 0; i < pixelCount; i++) {
-		pixelContainer.push(<Pixel key={i} />);
-	}
+for (var i = 0; i < pixelCount; i++) {
+	pixelContainer.push(<Pixel key={i} />);
 }
 export default class App extends React.Component{
 	render(){
