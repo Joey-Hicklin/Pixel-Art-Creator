13e64fd16b14355d80c44e51643b1796405d5a31
Joey-Hicklin <j.hicklin87@yahoo.com>
2017-07-28 17:38:54 -0700

    node bundle 1

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	devtool: 'inline-source-map',
+	devtool: 'cheap-source-map',
 	entry: [
-		'webpack-dev-server/client?http://127.0.0.1:8080/',
-		'webpack/hot/only-dev-server',
 		'./src'
 	],
 	output: {
@@ -13,20 +11,25 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	resolve: {
-		modulesDirectories: ['node_modules', 'src'],
-		extensions: ['', '.js']
+		moduleDirectories: ['node_modules', 'src'],
+		extensions: ['', '.js']	
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.jsx?/,
 				exclude: /node_modules/,
-				loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+				loaders: ['babel?presets[]=react,presets[]=es2015']
 			}
 		]
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
 	]
+	
 };
